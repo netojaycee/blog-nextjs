45bd0327a9a1a053de04b8b89bfcea28e8edd389
netojaycee <117773169+netojaycee@users.noreply.github.com>
2023-09-27 16:32:32 +0100

    update

--- app/admin/blog/edit-blog/[id]/page.jsx
+++ app/admin/blog/edit-blog/[id]/page.jsx
@@ -10,7 +10,7 @@ import { FormEvent, useEffect, useState  } from "react";
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
 
-export default function EditBlog({ blog}) {
+export default function EditBlog() {
   const [blogData, setBlogData] = useState({});
 
 
