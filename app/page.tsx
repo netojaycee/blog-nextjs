112764a8fd0265694e5747fa24b22016ba2d7ffc
Jerryrni <jerryeni3@gmail.com>
2023-09-30 14:35:40 +0100

    Homepage update

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,22 @@
 'use client'
+import Hero from '../components/Home/Hero'
+import Features from '../components/Home/Features'
+import Mental from '../components/Home/Mental'
+import Whats from '../components/Home/Whats'
+import Faqs from '../components/Home/Faqs'
+import Footer from '../components/Home/Footer'
 
 export default function Home() {
   return (
     <main className=''>
-      <h1 className='title p-0 mt-8 text-3xl font-bold underline'>home page</h1>
+      <h1 className='title p-0 mt-8 text-3xl font-bold underline'>
+        <Hero />
+        <Features />
+        <Mental />
+        <Whats />
+        <Faqs/>
+        <Footer/>
+      </h1>
     </main>
   )
 }
---
+++ components/Home/Mental.jsx
@@ -0,0 +1,30 @@
+import { Card } from '@material-tailwind/react';
+import Image from 'next/image';
+import React from 'react';
+
+const Mental = () => {
+    return (
+        <section className="w-full relative flex flex-col  bg-primary-500 bg[url(/images/modal.png)] ">
+            <div className="flex md:flex-row flex-col-reverse md:px-20 w-full md:mt-20 mt-40 justify-between items-center py-10 relative gap-10 object-contain">
+
+
+                <div className="w-full object-fit ">
+                    <Image src={"/images/brain-1.png"} height={700} width={700} className="object-cover rounded-3xl" alt="man wearing glasses" />
+                </div>
+                <div className="text-left space-y-8 w-full">
+                    <h1 className="font-bold text-2xl md:text-4xl text-primary">
+                        Get your own mental health chatbot.
+                    </h1>
+                    <p className="font-normal text-base leading-10">
+                        Do you have a website or mobile application?
+                        You can integrate a mental health chatbot into your website or application. Depending on your needs, clients can book appointments, learn about common mental health conditions and receive basic mental health support with a chatbot. You can also use a chatbot as a means to generate leads to get new clients.
+                    </p>
+                </div>
+            </div>
+            
+        </section>
+
+    );
+};
+
+export default Mental;
